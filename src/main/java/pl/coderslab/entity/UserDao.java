075fba964fb422c724378f22d79eba215296bb0e
Mirosław Nowak <mnowak0995@gmail.com>
2024-02-10 10:12:33 +0100

    working files - user, userDao, footer, header. Also database connection and local server are done

---
+++ src/main/java/pl/coderslab/entity/UserDao.java
@@ -0,0 +1,117 @@
+package pl.coderslab.entity;
+
+import org.mindrot.jbcrypt.BCrypt;
+import pl.coderslab.utils.DbUtil;
+
+import java.sql.*;
+import java.util.Arrays;
+
+public class UserDao {
+
+    private static final String CREATE_USER_QUERY = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+    private static final String READ_QUERY = "SELECT * FROM users WHERE users.id = ?";
+
+    private static final String DELETE_QUERY = "DELETE FROM users WHERE id = ?";
+    private static final String PRINT_ALL_QUERY = "SELECT * FROM tableName";
+    private static final String UPDATE_QUERY = "UPDATE users SET users.email = ?, users.username = ?, users.password = ? WHERE users.id = ?";
+
+
+    public void update(User user) {
+        try {
+            Connection conn = DbUtil.getConnection();
+            PreparedStatement statement = conn.prepareStatement(UPDATE_QUERY);
+            statement.setString(1, user.getEmail());
+            statement.setString(2, user.getUserName());
+            statement.setString(3, hashPassword(user.getPassword()));
+            statement.setInt(4, user.getId());
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.getMessage();
+        }
+    }
+
+    public User[] findAll(String tableName) {
+        User[] userArr;
+        userArr = new User[0];
+        try {
+
+            Connection conn = DbUtil.getConnection();
+            PreparedStatement statment = conn.prepareStatement(PRINT_ALL_QUERY.replace("tableName", tableName));
+            ResultSet resutleSet = statment.executeQuery();
+            ResultSet resultSet = statment.executeQuery();
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt(1));
+                user.setEmail(resultSet.getString(2));
+                user.setUserName(resultSet.getString(3));
+                user.setPassword(resultSet.getString(4));
+                 userArr = addToArray(user, userArr);
+            }
+        } catch (SQLException e) {
+            e.getMessage();
+        }
+        return userArr;
+    }
+
+    private User[] addToArray(User u, User[] users) {
+        User[] tmpUsers = Arrays.copyOf(users, users.length + 1);
+        tmpUsers[users.length] = u;
+        return tmpUsers;
+    }
+
+    public void delete (int userId) {
+        try {
+            Connection conn = DbUtil.getConnection();
+            PreparedStatement statement = conn.prepareStatement(DELETE_QUERY);
+            statement.setInt(1, userId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.getMessage();
+        }
+    }
+
+    public User read(int userId) {
+        User user = new User();
+        try {
+            Connection conn = DbUtil.getConnection();
+            PreparedStatement statement = conn.prepareStatement(READ_QUERY);
+            statement.setInt(1, userId);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                user.setId(resultSet.getInt(1));
+                user.setEmail(resultSet.getString(2));
+                user.setUserName(resultSet.getString(3));
+                user.setPassword(resultSet.getString(4));
+            }
+        } catch (SQLException e) {
+            e.getMessage();
+            return null;
+        }
+        return user;
+    }
+
+    public User create(User user) {
+        try (Connection conn = DbUtil.getConnection()) {
+            PreparedStatement statement =
+                    conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getUserName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, hashPassword(user.getPassword()));
+            statement.executeUpdate();
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()) {
+                user.setId(resultSet.getInt(1));
+            }
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+
+    }
+}
+
---
+++ src/main/java/pl/coderslab/users/UserList.java
@@ -0,0 +1,21 @@
+package pl.coderslab.users;
+
+import pl.coderslab.entity.UserDao;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "UserList", value = "/user/list")
+public class UserList extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        UserDao userDao = new UserDao();
+        request.setAttribute("users", userDao.findAll("users"));
+
+        request.getServletContext().getRequestDispatcher("/users/list.jsp")
+                .forward(request, response);
+    }
+}
