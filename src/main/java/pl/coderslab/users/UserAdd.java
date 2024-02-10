b39a6f9d83c13815d79362e3bad60446f4fe1663
Mirosław Nowak <mnowak0995@gmail.com>
2024-02-10 13:43:11 +0100

    renamed file

--- src/main/java/pl/coderslab/users/UserAdd.java
+++ src/main/java/pl/coderslab/users/UserAdd.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class UserAdd extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/users/create.jsp").forward(request, response);
+        request.getRequestDispatcher("/users/add.jsp").forward(request, response);
     }
 
     @Override
